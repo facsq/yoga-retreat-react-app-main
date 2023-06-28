cd5d9c23917532783e4cf492e15bbb9f5498776c
Jasraj S. Juneja <jasraj.209301235@muj.manipal.edu>
2023-06-28 16:51:42 +0530

    updated rishikesh

---
+++ src/components/Rishikesh/7Day/Package.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import Day1 from './Day.jpg'
+const Package = () => {
+  return (
+    <div>
+      <div className='mt-14 mx-auto px-4 bg-litegreen rounded-2xl drop-shadow-xl py-8
+      md:flex md:w-[90%]
+      xl:w-5/6'>
+        <div className='mx-2
+        md:w-[40%]'>
+          <img src={Day1} className='rounded-2xl
+          md:w-full md:h-full md:object-cover' alt='Day1'/>
+        </div>
+        <div className='w-5/6 mx-auto
+        md:w-1/2'>
+          <h1 className='text-center mx-2 font-bold text-xl
+          sm:text-2xl md:text-xl
+          xl:text-3xl'>Day 1: Welcome</h1>
+          <ul className='list-disc text-[1.08rem] mt-2 ml-2
+          sm:text-[1.35rem] md:text-[1.08rem]
+          xl:text-[1.45rem] xl:mt-6 '>
+            <li> in Traditional Indian Manner </li>
+            <li> Refreshments </li>
+            <li> 3:30 PM: Welcome kit and Retreat Orientation, introduction and Demonstrations of Retreat by Yoga Guru. </li>
+            <li> 5:00 PM Ganga Aarti at Triveni Ghat </li>
+            <li> 8:00pm Dinner at Ayushman Retreat </li>
+            <li> 10pm Lights out </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='mt-14 mx-auto px-4 bg-litegreen rounded-2xl drop-shadow-xl py-8
+      md:flex md:w-[90%]
+      xl:w-5/6'>
+        <div className='mx-2
+        md:w-[40%]'>
+          <img src={Day1} className='rounded-2xl
+          md:w-full md:h-full md:object-cover' alt='Day1'/>
+        </div>
+        <div className='w-5/6 mx-auto
+        md:w-1/2'>
+          <h1 className='text-center mx-2 font-bold text-xl
+          sm:text-2xl md:text-xl
+          xl:text-3xl'>Day 2: introduction</h1>
+          <ul className='list-disc text-[1.08rem] mt-2 ml-2
+          sm:text-[1.35rem] md:text-[1.08rem]
+          xl:text-[1.45rem] xl:mt-6 '>
+            <li> in Traditional Indian Manner </li>
+            <li> Refreshments </li>
+            <li> 3:30 PM: Welcome kit and Retreat Orientation, introduction and Demonstrations of Retreat by Yoga Guru. </li>
+            <li> 5:00 PM Ganga Aarti at Triveni Ghat </li>
+            <li> 8:00pm Dinner at Ayushman Retreat </li>
+            <li> 10pm Lights out </li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div className='mt-14 mx-auto px-4 bg-litegreen rounded-2xl drop-shadow-xl py-8
+      md:flex md:w-[90%]
+      xl:w-5/6'>
+        <div className='mx-2
+        md:w-[40%]'>
+          <img src={Day1} className='rounded-2xl
+          md:w-full md:h-full md:object-cover' alt='Day1'/>
+        </div>
+        <div className='w-5/6 mx-auto
+        md:w-1/2'>
+          <h1 className='text-center mx-2 font-bold text-xl
+          sm:text-2xl md:text-xl
+          xl:text-3xl'>Day 3: A Walk to Remember</h1>
+          <ul className='list-disc text-[1.08rem] mt-2 ml-2
+          sm:text-[1.35rem] md:text-[1.08rem]
+          xl:text-[1.45rem] xl:mt-6 '>
+            <li> in Traditional Indian Manner </li>
+            <li> Refreshments </li>
+            <li> 3:30 PM: Welcome kit and Retreat Orientation, introduction and Demonstrations of Retreat by Yoga Guru. </li>
+            <li> 5:00 PM Ganga Aarti at Triveni Ghat </li>
+            <li> 8:00pm Dinner at Ayushman Retreat </li>
+            <li> 10pm Lights out </li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div className='mt-14 mx-auto px-4 bg-litegreen rounded-2xl drop-shadow-xl py-8
+      md:flex md:w-[90%]
+      xl:w-5/6'>
+        <div className='mx-2
+        md:w-[40%]'>
+          <img src={Day1} className='rounded-2xl
+          md:w-full md:h-full md:object-cover' alt='Day1'/>
+        </div>
+        <div className='w-5/6 mx-auto
+        md:w-1/2'>
+          <h1 className='text-center mx-2 font-bold text-xl
+          sm:text-2xl md:text-xl
+          xl:text-3xl'>Day 1: Welcome</h1>
+          <ul className='list-disc text-[1.08rem] mt-2 ml-2
+          sm:text-[1.35rem] md:text-[1.08rem]
+          xl:text-[1.45rem] xl:mt-6 '>
+            <li> in Traditional Indian Manner </li>
+            <li> Refreshments </li>
+            <li> 3:30 PM: Welcome kit and Retreat Orientation, introduction and Demonstrations of Retreat by Yoga Guru. </li>
+            <li> 5:00 PM Ganga Aarti at Triveni Ghat </li>
+            <li> 8:00pm Dinner at Ayushman Retreat </li>
+            <li> 10pm Lights out </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Package
--- src/components/Rishikesh/Rishikesh.js
+++ src/components/Rishikesh/Rishikesh.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import TamiaImg from '../../assets/rishikesh.png'
+import location from '../../assets/loaction.svg'
+import call from '../../assets/call.svg'
+import mail from '../../assets/mail.svg'
+import Day4 from './4Day/Package'
+import Day7 from './7Day/Package'
+import Day11 from './11Day/Package'
 
 const Rishikesh = () => {
+
+  const [plan, setPlan] = useState("4day");
+
   return (
-    <div>
-      
-    </div>
+    <section className='absolute top-24 bg-white'>
+      <div className='relative h-[38rem] lg:h-[42rem] xl:mx-16 lg:mx-16 md:mx-16 mx-4 2xl:mx-24 animate-fade animate-once animate-duration-500 animate-ease-linear rounded-3xl drop-shadow-2xl overflow-hidden' >
+        <div className='absolute w-full h-16 bg-black bg-opacity-40 '>
+          <h1 className='text-white mt-3 ml-3 text-2xl font-medium text-center
+          sm:text-3xl md:ml-4
+          lg:ml-8'>Rishikesh, Uttarakhand</h1>
+        </div>
+        <div className='absolute top-36 w-10 left-6
+        lg:'><img src={location} alt='location'/></div>
+        <div className='absolute top-56 w-10 left-6
+        lg:'><img src={call} alt='call'/></div>
+        <div className='absolute top-80 w-10 left-6
+        lg:'><img src={mail} alt='mail'/></div>
+
+        <img src={TamiaImg} className='w-full h-full object-cover' alt='Tamia'/>
+
+        <div className='absolute bottom-0 h-48 text-white bg-black bg-opacity-40
+        sm:'>
+          {/* <h1 className='text-white mt-3 ml-3 text-2xl font-medium text-center
+          sm:text-3xl md:ml-4
+          lg:ml-8'>Tamia, Madhya Pradesh</h1> */}
+          <p className='realtive text-sm w-5/6 h-5/6 font-sans text-center mx-auto my-5 
+           sm:text-base sm:my-8
+           lg:text-lg lg:w-5/6 lg:my-10'>Yoga Retreat is a gathering of people who are joining together to practice yoga in a group setting.
+          Yoga Retreat is a gathering of people who are joining together to practice yoga in a group setting.
+          Yoga Retreat is a gathering of people who are joining together to practice yoga in a group setting.</p>
+        </div>
+      </div>
+
+      <div className='mt-3'>
+        <div>
+          <h1 className='text-[1.75rem] text-sitegreen mt-14 font-extrabold text-center'>Yoga Retreat Packages</h1>
+        </div>
+        <div className='mx-4 flex flex-wrap md:justify-center'>
+          <button className= {`border-2 w-48 h-12 mx-auto my-2 rounded-xl lg:mx-4 focus:bg-sitegreen ${plan==='4day'? 'bg-sitegreen text-white border-litegreen font-semibold':'hover:bg-litegreen border-sitegreen bg-white text-black '}`} onClick={()=> setPlan('4day')}>4-Day Basic Package</button>
+          <button className= {`border-2 w-48 h-12 mx-auto my-2 rounded-xl lg:mx-4 focus:bg-sitegreen ${plan==='7day'? 'bg-sitegreen text-white border-litegreen font-semibold':'hover:bg-litegreen border-sitegreen bg-white text-black'}`} onClick={()=> setPlan('7day')}>4-Day Basic Package</button>
+          <button className={`border-2 w-48 h-12 mx-auto my-2  rounded-xl lg:mx-4 focus:bg-sitegreen ${plan==='11day'? 'bg-sitegreen text-white border-litegreen font-semibold':'hover:bg-litegreen border-sitegreen bg-white text-black'}`} onClick={()=> setPlan('11day')}>4-Day Basic Package</button>
+
+        </div>
+        <br/>
+        <div className='mx-14'>
+          <div className={`${plan==='4day'? 'block': 'hidden'} 
+          animate-fade-down animate-once animate-duration-500 animate-ease-linear"`}>
+            <Day4 />
+          </div>
+          <div className={`${plan==='7day'? 'block': 'hidden'}
+          animate-fade-down animate-once animate-duration-300 animate-ease-linear`}>
+            <Day7 />
+          </div>
+          <div className={`${plan==='11day'? 'block': 'hidden'}
+          animate-fade-down animate-once animate-duration-300 animate-ease-linear`}>
+            <Day11/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
